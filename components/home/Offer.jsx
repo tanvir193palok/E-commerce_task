2c6d10faeff2bb069b14fd126d1d4510f0a6fd5a
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-11-25 23:23:38 +0600

    adjusted padding

--- components/home/Offer.jsx
+++ components/home/Offer.jsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 
 const Offer = () => {
   return (
-    <div className="w-[360px] lg:w-[1280px] h-[633px] lg:h-[450px] bg-[#EEECFB] px-4 lg:mx-28 my-2 flex flex-col lg:flex-row justify-center items-center lg:justify-between lg:px-[75px]">
-      {/* Text */}
-      <div className="flex-1 mt-10 lg:mt-0 px-6 text-center lg:text-left">
+    <div className="w-[340px] lg:w-[1280px] h-auto lg:h-[450px] bg-[#EEECFB] flex flex-col lg:flex-row justify-center items-center lg:justify-between mx-4 lg:px-[75px] py-4 lg:py-0">
+      {/* Text Section */}
+      <div className="flex-1 text-center lg:text-left px-4">
         <p className="text-[24px] font-primary text-primary">Big Deal</p>
-        <p className="w-[328px] lg:w-full text-[28px] lg:text-[48px] font-semibold">
+        <p className="text-[28px] lg:text-[48px] font-semibold">
           <span className="text-primary">30% </span>off for New Customers
         </p>
 
-        <div className="mt-[8px] flex items-center justify-center lg:justify-start">
+        <div className="mt-4 flex items-center justify-center lg:justify-start">
           <button className="w-[149px] h-[48px] bg-primary hover:bg-primary-dark text-white rounded-[28px]">
             Shop Now
           </button>
-          <div className="flex items-center justify-center w-12 h-12 bg-primary rounded-full">
+          <div className="flex items-center justify-center w-12 h-12 bg-primary rounded-full ml-4">
             <Image
               src="/arrow.png"
               alt="arrow"
@@ -26,8 +26,8 @@ const Offer = () => {
         </div>
       </div>
 
-      {/* Image */}
-      <div className="w-[360px] lg:w-[427px] h-[369px] lg:h-[450px]">
+      {/* Image Section */}
+      <div className="flex-shrink-0 w-[350px] lg:w-[427px] h-[369px] lg:h-[450px] mt-4 lg:mt-0">
         <Image
           src="/offer.png"
           height={800}
